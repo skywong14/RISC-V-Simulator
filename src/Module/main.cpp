2c03461272095eaab9cc8545e389cb70b063a033
skywong14 <skywong0014@gmail.com>
2024-08-01 02:39:18 +0800

    A test with buffer = 1

--- src/Module/main.cpp
+++ src/Module/main.cpp
@@ -9,7 +9,7 @@
 int commit_cnt = 0;
 
 int main() {
-    freopen("input.txt", "r", stdin);
+//    freopen("input.txt", "r", stdin);
 //    freopen("output.txt", "w", stdout);
     Memory mem;
     mem.load_memory_from_stream(std::cin);
